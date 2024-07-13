7ca4e49f0bb8c9abf7ddacdf4beec6567c8dc18d
Clara-13 <clararandria@gmail.com>
2024-07-13 23:31:41 +0200

    ajout page contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,6 +7,7 @@ export const allRoutes = [
     new Route("/services", "Nos services","/pages/services.html"),
     new Route("/animaux", "Nos Animaux","/pages/animaux.html"),
     new Route("/signin", "Connexion","/pages/signin.html"),
+    new Route("/contact", "Contact","/pages/contact.html"),
 
 ];
 
