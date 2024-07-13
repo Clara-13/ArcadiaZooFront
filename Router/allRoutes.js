dc0d7da5e8a3ce45a6eac681c03faf1bce066461
Clara-13 <clararandria@gmail.com>
2024-07-13 13:36:05 +0200

    ajout habitats

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/animaux", "Nos Animaux", "/pages/animaux.html"),
+    new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
