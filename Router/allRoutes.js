979a59bfda7e5fcf1c0bdf11d3303c3e543f65cf
Clara-13 <clararandria@gmail.com>
2024-07-16 21:23:01 +0200

    ajout pages compte et create-account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,10 +6,10 @@ export const allRoutes = [
     new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
     new Route("/services", "Nos services","/pages/services.html"),
     new Route("/animaux", "Nos Animaux","/pages/animaux.html"),
-    new Route("/signin", "Connexion","/pages/signin.html"),
+    new Route("/signin", "Connexion","/pages/auth/signin.html"),
     new Route("/contact", "Contact","/pages/contact.html"),
-    new Route("/account", "Mon Compte","/pages/compte.html"),
-    new Route("/create-account", "Création de compte","/pages/create-account.html"),
+    new Route("/account", "Mon Compte","/pages/auth/compte.html"),
+    new Route("/create-account", "Création de compte","/pages/auth/create-account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
