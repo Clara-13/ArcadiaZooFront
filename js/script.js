e71707ae7f609ffd3afc699f975cf16486e49d16
Clara-13 <clararandria@gmail.com>
2024-07-21 17:08:28 +0200

    ajout id footer

--- js/script.js
+++ js/script.js
@@ -189,3 +189,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+// fonction pour que cela redirige de manière fluide
+document.addEventListener("DOMContentLoaded", function() {
+    if(window.location.hash) {
+        const hash = window.location.hash.substring(1);
+        const element = document.getElementById(hash);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+});
